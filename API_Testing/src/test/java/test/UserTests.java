b40a2f8c0e11035f8cf58c78a4970d54633e1080
Vasif Sultanov <cdm_89@mail.ru>
2024-05-02 16:30:10 +0500

    02.05.2024

---
+++ API_Testing/src/test/java/test/UserTests.java
@@ -0,0 +1,70 @@
+package test;
+
+import com.github.javafaker.Faker;
+import endpoints.UserEndPoints;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import payload_POJO.User;
+
+public class UserTests {
+    Faker faker;
+    User userPayload;
+
+
+    @BeforeClass
+    public void setupDate() {
+        faker = new Faker();
+        userPayload = new User();
+
+        userPayload.setId(faker.idNumber().hashCode());
+        userPayload.setUsername(faker.name().username());
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+        userPayload.setPassword(faker.internet().password());
+        userPayload.setPhone(faker.phoneNumber().cellPhone());
+    }
+
+
+    @Test (priority = 1)
+    public void testPOSTuser() {
+        Response response = UserEndPoints.createUSer(userPayload);
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("----------------------------------------");
+
+    }
+
+    @Test (priority = 2)
+    public void testGetUserByName() {
+        Response response = UserEndPoints.readUser(userPayload.getUsername());
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println("----------------------------------------");
+    }
+
+    @Test (priority = 3)
+    void updateUserByName() {
+
+        // update data using payload
+        userPayload.setUsername(faker.name().username());
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+
+
+        Response response = UserEndPoints.updateUser(userPayload, this.userPayload.getUsername());
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(),200);
+
+        // for checking how updating was passed
+        response = UserEndPoints.readUser(this.userPayload.getUsername());
+        response.then().log().body();
+        System.out.println("----------------------------------------");
+    }
+
+}
