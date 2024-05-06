915b56ae058e593f5be4eae9ecc50cab4fba1218
Vasif Sultanov <cdm_89@mail.ru>
2024-05-06 18:17:42 +0500

    06.05.2024

--- API_Testing/src/test/java/utilities/DataProviders.java
+++ API_Testing/src/test/java/utilities/DataProviders.java
@@ -9,40 +9,76 @@ import java.util.Arrays;
 public class DataProviders {
 
     @DataProvider(name = "Data")
-    public String[][] getAllData() throws IOException {
+    public Object[][] getAllData() {
         String path = System.getProperty("user.dir") + "//testData/Data.xlsx";
         XLUtility xl = new XLUtility(path);
 
-        int rowNum = xl.getRowCount("Sheet1");
-        int columCount = xl.getCellCount("Sheet1", 1);
+        String[][] apidata = new String[0][];
+        try {
+            int rowNum = xl.getRowCount("Sheet1");
+            int columCount = xl.getCellCount("Sheet1", 1);
 
-        String[][] apidata = new String[rowNum][columCount];
+            apidata = new String[rowNum][columCount];
 
-        for (int i = 1; i <= rowNum; i++) {
-            for (int j = 0; j < columCount; j++) {
-                apidata[i - 1][j] = xl.getCellData("Sheet1", i, j);
+            for (int i = 1; i <= rowNum; i++) {
+                for (int j = 0; j < columCount; j++) {
+                    apidata[i - 1][j] = xl.getCellData("Sheet1", i, j);
+                }
             }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
         return apidata;
     }
 
-    @DataProvider(name = "UsersName")
-    public String[] getUserNames() throws IOException {
+/*    @DataProvider(name = "UsersName")
+    public Object[][] getUserNames() {
         String path = System.getProperty("user.dir") + "//testData/Data.xlsx";
         XLUtility xl = new XLUtility(path);
 
-        int rowNum = xl.getRowCount("Sheet1");
-        String[] apidata = new String[rowNum];
+        Object[][] apidata = new Object[0][];
+        try {
+            int rowNum = xl.getRowCount("Sheet1");
+            apidata = new Object[rowNum][];
 
-        for (int i = 1; i <= rowNum; i++) {
-            apidata[i - 1] = xl.getCellData("Sheet1", i, 1);
+            for (int i = 1; i <= rowNum; i++) {
+                apidata[i - 1][0] = xl.getCellData("Sheet1", i, 1);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         return apidata;
+    }*/
+
+    @Test/*(name = "UsersName")*/
+    public void getUserNames() {
+        String path = System.getProperty("user.dir") + "//testData/Data.xlsx";
+        XLUtility xl = new XLUtility(path);
+
+        String[][] apidata;
+        try {
+            int rowNum = xl.getRowCount("Sheet1");
+
+            System.out.println(rowNum);
+
+            apidata = new String[rowNum][];
+
+            for (int i = 1; i <= rowNum; i++) {
+                apidata[i - 1][0] = xl.getCellData("Sheet1", i, 1);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Arrays.toString(apidata));
     }
 
-    @Test (dataProvider = "UsersName",dataProviderClass = DataProviders.class)
-    void read() throws IOException {
-        String[] a = getUserNames();
+
+
+   // @Test(dataProvider = "UsersName",dataProviderClass = DataProviders.class)
+    /*void read() throws IOException {
+        Object[][] a = getUserNames();
         System.out.println(Arrays.toString(a));
-    }
+    }*/
 }
